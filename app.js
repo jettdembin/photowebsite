f254a723ab9223c58e349a44e73782e328187a91
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-06-07 10:50:12 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const auth = "563492ad6f91700001000001436fc2271b5348849b4be3c64df28d78";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
