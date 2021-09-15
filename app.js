4357f0e5cede5aec7ab76222d6c8f027c71df6b2
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-09-15 11:46:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const auth = "563492ad6f91700001000001436fc2271b5348849b4be3c64df28d78";
-const gallery = document.querySelector(".gallery");
-const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+var picturesCntr = document.querySelector("#pictures-cntr");
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+        var res = JSON.parse(xhttp.responseText);
+        var photos = res.photos.map(function(photo) {
+            return photo.src
+            // var image = document.createElement('div');
+            // image.innerHTML = `<img src=${item.src.orginal}>
+            // `
+            // res.appendChild(image);
+        })
+        photos.forEach(function(item) {
+            var image = document.createElement('div');
+            image.innerHTML = `<img src=${item.small}>
+            `
+            picturesCntr.appendChild(image);
+        })
+    }
+};
+xhttp.open("GET", "https://api.pexels.com/v1/search?query=surf", true);
+xhttp.setRequestHeader('Authorization', '563492ad6f917000010000018a35618275544479aa4dc8840d5ce33a');
+xhttp.send();
